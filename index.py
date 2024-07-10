352a130be2541b408b05b73a3b693db613c564d9
ZION BIOM <102071095+ayan2235@users.noreply.github.com>
2024-07-10 16:42:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,32 +7,12 @@ from fastapi.responses import JSONResponse
 from bins import mydict, get_bin_info
 import re
 from pydantic import BaseModel
-from py_adyen_encrypt import encryptor
 import urllib
 
-
-def adyen_enc(cc, mes, ano, cvv, ADYEN_KEY, adyen_version):
-    enc = encryptor(ADYEN_KEY)
-    enc.adyen_version = adyen_version
-    enc.adyen_public_key = ADYEN_KEY
-
-    card = enc.encrypt_card(card=cc, cvv=cvv, month=mes, year=ano)
-    month = card['month']
-    year = card['year']
-    cvv = card['cvv']
-    card = card['card']
-
-    card = urllib.parse.quote_plus(card)
-    Month = urllib.parse.quote_plus(month)
-    Year = urllib.parse.quote_plus(year)
-    cvv = urllib.parse.quote_plus(cvv)
-    return card, Month, Year,cvv
-
-
 app = FastAPI(debug=True, 
-              title="Bin lookup And Adyen Generator By Roldexverse.", 
+              title="Bin lookup By Ayan.", 
               redoc_url=None,
-              description=" Feel free to use. made by @roldexverse for roldexverse subscribers.")
+              description=" Feel free to use. made by @Ay_4n.")
 
 
 
@@ -67,10 +47,10 @@ async def bin(bin1):
     return {
         'bin': bin,
         'bank': bin_data['bank_name'],
-        'country_iso': bin_data['iso'],
+        'country_iso2': bin_data['iso'],
         'country': bin_data['country'],
         'flag': bin_data['flag'],
-        'vendor': bin_data['vendor'],
+        'brand': bin_data['vendor'],
         'type': bin_data['type'],
         'level': bin_data['level'],
         'prepaid': bin_data['prepaid'],                                                                               
@@ -79,25 +59,5 @@ async def bin(bin1):
 
 @app.get("/")
 async def start():
-    return RedirectResponse("http://www.github.com/r0ld3x/adyen-enc-and-bin-info")
+    return RedirectResponse("http://www.github.com/")
 
-
-class Item(BaseModel):
-    card: str
-    month: str
-    year: str
-    cvv: str
-    adyen_version: str
-    adyen_key: str
-
-
-@app.post("/adyen/")
-async def adyen(item: Item):
-    cc, mes, ano, cvv = adyen_enc(
-        item.card, item.month, item.year, item.cvv, item.adyen_key, item.adyen_version)
-    return {
-        'card': cc,
-        'month': mes,
-        'year': ano,
-        'cvv': cvv
-    }
